c91386920002d2deae51b955b1d8370b1e863df9
TheZLing <95129507+TheZLing@users.noreply.github.com>
2022-06-12 06:34:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
+/**
+ * Welcome to the Brainfucker++, a great brainfuck & brainfuck derivatives' interpreter.
+ * You can go to the "https://esolangs.org/wiki/Brainfucker%2B%2B" to learn this interpreter.
+ * Now. let's go!
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+
+
 
 int main(int argc, char *argv[]) {
     
